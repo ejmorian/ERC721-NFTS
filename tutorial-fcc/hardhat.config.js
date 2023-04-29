48cf6a117b3e1b450dae6b876dcd730af7a5a47f
ejmorian <ejmorian@gmail.com>
2023-04-29 15:29:10 +0800

    reset

--- tutorial-fcc/hardhat.config.js
+++ tutorial-fcc/hardhat.config.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const MUMBAI_APIKEY = process.env.MUMBAI_APIKEY;
 
 
 module.exports = {
@@ -18,12 +20,15 @@ module.exports = {
       url: SEPOLIA_URL,
       chainId: 11155111,
       accounts: [PRIVATE_KEY]
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_APIKEY}`,
+      chainId: 80001,
+      accounts: [PRIVATE_KEY]
     }
   },
   etherscan: {
-    
     apiKey: {sepolia : ETHERSCAN_API_KEY,}
-
   },
   mocha: {
     setTimeout: 100000,
